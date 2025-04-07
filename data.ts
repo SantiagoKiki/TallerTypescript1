1b799cd448a9349343fc5f01166f66e4b2cd9639
Kiki Santiago Quiroz <123739640+SantiagoKiki@users.noreply.github.com>
2025-04-06 21:21:31 -0500

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,33 @@
+import { Serie } from './Serie.js';
+
+export const series: Serie[] = [
+  new Serie(1, "Breaking Bad", "AMC", 5, 
+    "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer.",
+    "https://www.amc.com/shows/breaking-bad", 
+    "https://m.media-amazon.com/images/M/MV5BMzU5ZGYzNmQtMTdhYy00OGRiLTg0NmQtYjVjNzliZTg1ZGE4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
+    
+  new Serie(2, "Orange Is the New Black", "Netflix", 6, 
+    "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary.", 
+    "https://www.netflix.com/co/title/70242311", 
+    "https://m.media-amazon.com/images/M/MV5BNjRjMTAyYjUtZDk0Mi00OGQ5LWJjNzMtMzBhNDMwMmI5NzVjXkEyXkFqcGc@._V1_.jpg"),
+    
+  new Serie(3, "Game of Thrones", "HBO", 7, 
+    "American fantasy drama.", 
+    "https://www.hbo.com/game-of-thrones", 
+    "https://m.media-amazon.com/images/M/MV5BOGY3NTg1ODMtOGIzZS00YWFiLTgzYzktNzBiYWZkYjcwNGRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
+
+  new Serie(4, "The Big Bang Theory", "CBS", 12, 
+    "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else.", 
+    "https://www.cbs.com/shows/big_bang_theory/about/", 
+    "https://m.media-amazon.com/images/M/MV5BZjgzY2QyNzItNDhhYi00ZWIwLWFjN2UtZDJkN2MxYWNjMmJjXkEyXkFqcGc@._V1_.jpg"),
+    
+  new Serie(5, "Sherlock", "BBC", 4, 
+    "Sherlock depicts consulting detective Sherlock Holmes solving various mysteries in modern-day London.", 
+    "https://www.bbc.co.uk/programmes/b018ttws", 
+    "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
+
+  new Serie(6, "A Very English Scandal", "BBC", 2, 
+    "A fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.", 
+    "https://www.bbc.co.uk/programmes/p065smy4", 
+    "https://m.media-amazon.com/images/M/MV5BNTQ5NDI0MTQ0MV5BMl5BanBnXkFtZTgwNDEzNTc1NTM@._V1_FMjpg_UX1000_.jpg")
+];
