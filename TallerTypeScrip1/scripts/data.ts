2f131ec92f4f36675fbf8953b181377dbb678aa4
SantiagoKiki <s.quirozp@uniandes.edu.co>
2025-04-06 23:38:27 -0500

    final commit :) cambio info para typescript 2

--- TallerTypeScrip1/scripts/data.ts
+++ TallerTypeScrip1/scripts/data.ts
@@ -1,33 +1,56 @@
-import { Serie } from './Serie.js';
-
-export const series: Serie[] = [
-  new Serie(1, "Breaking Bad", "AMC", 5, 
-    "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer.",
-    "https://www.amc.com/shows/breaking-bad", 
-    "https://m.media-amazon.com/images/M/MV5BMzU5ZGYzNmQtMTdhYy00OGRiLTg0NmQtYjVjNzliZTg1ZGE4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
-    
-  new Serie(2, "Orange Is the New Black", "Netflix", 6, 
-    "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary.", 
-    "https://www.netflix.com/co/title/70242311", 
-    "https://m.media-amazon.com/images/M/MV5BNjRjMTAyYjUtZDk0Mi00OGQ5LWJjNzMtMzBhNDMwMmI5NzVjXkEyXkFqcGc@._V1_.jpg"),
-    
-  new Serie(3, "Game of Thrones", "HBO", 7, 
-    "American fantasy drama.", 
-    "https://www.hbo.com/game-of-thrones", 
-    "https://m.media-amazon.com/images/M/MV5BOGY3NTg1ODMtOGIzZS00YWFiLTgzYzktNzBiYWZkYjcwNGRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
-
-  new Serie(4, "The Big Bang Theory", "CBS", 12, 
-    "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else.", 
-    "https://www.cbs.com/shows/big_bang_theory/about/", 
-    "https://m.media-amazon.com/images/M/MV5BZjgzY2QyNzItNDhhYi00ZWIwLWFjN2UtZDJkN2MxYWNjMmJjXkEyXkFqcGc@._V1_.jpg"),
-    
-  new Serie(5, "Sherlock", "BBC", 4, 
-    "Sherlock depicts consulting detective Sherlock Holmes solving various mysteries in modern-day London.", 
-    "https://www.bbc.co.uk/programmes/b018ttws", 
-    "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
-
-  new Serie(6, "A Very English Scandal", "BBC", 2, 
-    "A fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.", 
-    "https://www.bbc.co.uk/programmes/p065smy4", 
-    "https://m.media-amazon.com/images/M/MV5BNTQ5NDI0MTQ0MV5BMl5BanBnXkFtZTgwNDEzNTc1NTM@._V1_FMjpg_UX1000_.jpg")
+export const series = [
+  {
+      id: 1,
+      name: "Breaking Bad",
+      channel: "AMC",
+      seasons: 5,
+      description: "Walter White, un profesor de química, se convierte en fabricante de metanfetaminas tras un diagnóstico de cáncer.",
+      link: "https://www.amc.com/shows/breaking-bad",
+      imageUrl: "https://m.media-amazon.com/images/M/MV5BMzU5ZGYzNmQtMTdhYy00OGRiLTg0NmQtYjVjNzliZTg1ZGE4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+  },
+  {
+      id: 2,
+      name: "Orange Is the New Black",
+      channel: "Netflix",
+      seasons: 6,
+      description: "Piper Chapman entra a prisión por un crimen de juventud, mientras enfrenta la vida en Litchfield Penitentiary.",
+      link: "https://www.netflix.com/co/title/70242311",
+      imageUrl: "https://m.media-amazon.com/images/M/MV5BNjRjMTAyYjUtZDk0Mi00OGQ5LWJjNzMtMzBhNDMwMmI5NzVjXkEyXkFqcGc@._V1_.jpg"
+  },
+  {
+      id: 3,
+      name: "Game of Thrones",
+      channel: "HBO",
+      seasons: 7,
+      description: "Luchas de poder entre familias nobles por el control del Trono de Hierro en un mundo medieval fantástico.",
+      link: "https://www.hbo.com/game-of-thrones",
+      imageUrl: "https://m.media-amazon.com/images/M/MV5BOGY3NTg1ODMtOGIzZS00YWFiLTgzYzktNzBiYWZkYjcwNGRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+  },
+  {
+      id: 4,
+      name: "The Big Bang Theory",
+      channel: "CBS",
+      seasons: 12,
+      description: "Leonard y Sheldon son brillantes físicos, pero sus vidas sociales son desastrosas mientras conviven con sus amigos.",
+      link: "https://www.cbs.com/shows/big_bang_theory/about/",
+      imageUrl: "https://m.media-amazon.com/images/M/MV5BZjgzY2QyNzItNDhhYi00ZWIwLWFjN2UtZDJkN2MxYWNjMmJjXkEyXkFqcGc@._V1_.jpg"
+  },
+  {
+      id: 5,
+      name: "Sherlock",
+      channel: "BBC",
+      seasons: 4,
+      description: "Sherlock Holmes es un detective moderno que resuelve misterios complejos con su aguda mente y sus métodos innovadores.",
+      link: "https://www.bbc.co.uk/programmes/b018ttws",
+      imageUrl: "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+  },
+  {
+      id: 6,
+      name: "A Very English Scandal",
+      channel: "BBC",
+      seasons: 2,
+      description: "Historia de un escándalo político británico sobre un político que oculta un romance secreto con un hombre.",
+      link: "https://www.bbc.co.uk/programmes/p065smy4",
+      imageUrl: "https://m.media-amazon.com/images/M/MV5BNTQ5NDI0MTQ0MV5BMl5BanBnXkFtZTgwNDEzNTc1NTM@._V1_FMjpg_UX1000_.jpg"
+  }
 ];
